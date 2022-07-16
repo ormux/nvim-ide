1022a067fcd92d90dec0c75a4b28b50005b0b021
ormux <ormuseldesu@gmail.com>
2022-07-16 14:40:32 +1000

    neovim config options

---
+++ lua/core/options.lua
@@ -0,0 +1,31 @@
+local options = {
+	autoindent = true,
+	autoread = true,
+	backup = false,
+	clipboard = "unnamedplus",
+	errorbells = false,
+	expandtab = true,
+	hlsearch = true,
+	ignorecase = true,
+	incsearch = true,
+	laststatus = 2,
+	number = true,
+	relativenumber = true,
+	shiftwidth = 2,
+	smartcase = true,
+	smartindent = true,
+	softtabstop = 2,
+	swapfile = false,
+	tabstop = 2,
+	undofile = true,
+	wildmenu = true,
+	writebackup = false,
+  numberwidth = 2,
+  termguicolors = true,
+}
+
+for key, val in pairs(options) do
+	vim.opt[key] = val
+end
+
+vim.cmd [[set iskeyword+=-]] -- brackets = multi-line strings
