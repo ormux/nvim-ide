28218ab45b08f173d5f2bd3ff8f535ea847ac648
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-30 17:17:49 +1100

    Add formatters stylua, clang_format, blac, cpplint

--- lua/core/null-ls.lua
+++ lua/core/null-ls.lua
@@ -1,17 +1,25 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok or null_ls == nil then
-  vim.notify [[failed to load null-ls.]]
+  vim.notify([[failed to load null-ls.]])
   return
 end
 
 local formatting = null_ls.builtins.formatting
---local diagnostic = null_ls.builtins.diagnostics
---local completion = null_ls.builtins.completion
+local diagnostic = null_ls.builtins.diagnostics
+-- local completion = null_ls.builtins.completion
 
-null_ls.setup {
+null_ls.setup({
   sources = {
-    formatting.prettier.with {
-      extra_args = {"--single-quote", "--jsx-single-quote"}
-    },
-  }
-}
+    formatting.prettier.with({
+      extra_args = { "--single-quote", "--jsx-single-quote" }
+    }),
+    formatting.stylua,
+    formatting.clang_format.with({
+      filetypes = {
+        'c', 'cc', 'cpp'
+      }
+    }),
+    formatting.black,
+    diagnostic.cpplint,
+  },
+})
