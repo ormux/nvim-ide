9f60bcd395cbf7bd0688b837f463ca67ffe11a7f
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-05-06 13:10:43 +1000

    change structure of plugins to comply with lazy.nvim plugspec

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -1,122 +1,49 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path
-  })
-  print("Installed packer, please restart Neovim")
-  vim.cmd [[packadd packer.nvim]]
-end
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  vim.notfy "failed to load packer."
-  return
-end
-
--- https://github.com/wbthomason/packer.nvim#using-a-floating-window
-packer.init({
-  display = {
-    open_fn = require("packer.util").float
-  }
-})
-
-return packer.startup(
-  function(use)
-    use { "wbthomason/packer.nvim" }
-
-    -- Gruvbox Colorscheme
-    use { "luisiacc/gruvbox-baby", branch = "main" }
-
-    -- VSCode Colorscheme
-    use { "Mofiqul/vscode.nvim" }
-
-    use {
-      "nvim-lua/plenary.nvim",
-      commit = "253d34830709d690f013daf2853a9d21ad7accab"
-    }
-
-    use {
-      'kyazdani42/nvim-web-devicons',
-      commit = "c3c1dc4e36969370ff589b7025df8ec2e5c881a2"
-    }
-
-    -- Nvim Tree
-    use {
-      'kyazdani42/nvim-tree.lua',
-      tag = 'nightly',
-    }
-
-    -- Bufferline
-    use {
-      'akinsho/bufferline.nvim',
-      tag = "v3.*",
-    }
-
-    -- Treesitter
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      commit = "1ceaceb9dea9b0bac2162345ee11e47a44e07a70"
-    }
-
-    -- Snippet Engine
-    use {
-      "L3MON4D3/LuaSnip",
-    }
-
-    -- HTML Snippets
-    use {
-      "mattn/emmet-vim",
-      setup = function()
-        vim.g.user_emmet_mode = "n"
-        vim.g.user_emmet_leader_key = ","
-      end
-    }
-
-    -- Auto Completion
-    use { "hrsh7th/nvim-cmp", commit = "01f697a68905f9dcae70960a9eb013695a17f9a2" }
-    use { "hrsh7th/cmp-nvim-lsp", commit = "0e6b2ed705ddcff9738ec4ea838141654f12eeef" }
-    use { "hrsh7th/cmp-buffer", commit= "3022dbc9166796b644a841a02de8dd1cc1d311fa"}
-    use { "hrsh7th/cmp-path", commit = "91ff86cd9c29299a64f968ebb45846c485725f23" }
-    use { "hrsh7th/cmp-cmdline", commit = "8fcc934a52af96120fe26358985c10c035984b53" }
-    use { "saadparwaiz1/cmp_luasnip", commit = "18095520391186d634a0045dacaa346291096566" }
-    use { "rafamadriz/friendly-snippets", commit = "009887b76f15d16f69ae1341f86a7862f61cf2a1" }
-
-    -- Language Server Protocol
-    use { "williamboman/mason.nvim", commit = "51228a60d1a5017030429ba38f018ff27a460c76" }
-    use { "williamboman/mason-lspconfig.nvim", commit = "e4badf7984f7a5f0ac7dc10657dbedbd99a82f94" }
-    use { "neovim/nvim-lspconfig", commit = "a557dd4d493e6afba3c24ffeb96fa32695f00874" }
-
-    -- Null LS Formatter
-    use { "jose-elias-alvarez/null-ls.nvim", commit = "456cd2754c56c991c5e4df60a807d054c1bc7148" }
-
-    use {
-      "iamcco/markdown-preview.nvim",
-      run = function() vim.fn["mkdp#util#install"]() end,
-      setup = function() vim.g.mkdp_browser = 'qutebrowser' end,
-      opt = true,
-      ft = { "markdown" },
-    }
-
-    use { "numToStr/Comment.nvim", commit = "6821b3ae27a57f1f3cf8ed030e4a55d70d0c4e43" }
-
-    use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "a0f89563ba36b3bacd62cf967b46beb4c2c29e52" }
-
-    if PACKER_BOOTSTRAP then
-      require("packer").sync()
-    end
-  end
-)
+return {
+  { "luisiacc/gruvbox-baby",           branch = "main" },
+  "Mofiqul/vscode.nvim",
+  "nvim-lua/plenary.nvim",
+  "nvim-tree/nvim-web-devicons",
+  { "nvim-tree/nvim-tree.lua",         tag = "nightly" },
+  { "akinsho/bufferline.nvim",         tag = "v3.*" },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  "L3MON4D3/LuaSnip",
+  {
+    "mattn/emmet-vim",
+    init = function()
+      vim.g.user_emmet_mode = "n"
+      vim.g.user_emmet_leader_key = ","
+    end,
+    commit = "def5d57a1ae5afb1b96ebe83c4652d1c03640f4d",
+  },
+  { "hrsh7th/nvim-cmp",                  commit = "0f47dcaf7e39d64cd067e090e6f2eda6326e7366" },
+  { "hrsh7th/cmp-nvim-lsp",              commit = "0e6b2ed705ddcff9738ec4ea838141654f12eeef" },
+  { "hrsh7th/cmp-buffer",                commit = "3022dbc9166796b644a841a02de8dd1cc1d311fa" },
+  { "hrsh7th/cmp-path",                  commit = "91ff86cd9c29299a64f968ebb45846c485725f23" },
+  { "hrsh7th/cmp-cmdline",               commit = "5af1bb7d722ef8a96658f01d6eb219c4cf746b32" },
+  { "saadparwaiz1/cmp_luasnip",          commit = "18095520391186d634a0045dacaa346291096566" },
+  { "rafamadriz/friendly-snippets",      commit = "10f741908e56ce47cf6b3440e3f04b4fdc0da2f2" },
+  { "williamboman/mason.nvim",           commit = "057ac5ca159c83e302a55bd839a96ff1ea2396db" },
+  { "williamboman/mason-lspconfig.nvim", commit = "cec0e6dd534ade486b5eeb59ebcfa8d51c3e8c5e" },
+  { "neovim/nvim-lspconfig",             commit = "3bd4ca412982afa6f88bb7ef890660cee51c3e58" },
+  { "jose-elias-alvarez/null-ls.nvim",   commit = "33b853a3933eed3137cd055aac4e539e69832ad0" },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      vim.g.mkdp_browser = "qutebrowser"
+    end,
+    ft = { "markdown" },
+    lazy = true,
+    commit = "02cc3874738bc0f86e4b91f09b8a0ac88aef8e96",
+  },
+  "numToStr/Comment.nvim",
+  commit = "e1fe53117aab24c378d5e6deaad786789c360123",
+  { "JoosepAlviste/nvim-ts-context-commentstring", commit = "0bf8fbc2ca8f8cdb6efbd0a9e32740d7a991e4c3" },
+  { "nvim-telescope/telescope.nvim",               tag = "0.1.1" },
+  { "nvim-lualine/lualine.nvim",                   commit = "05d78e9fd0cdfb4545974a5aa14b1be95a86e9c9" },
+  { "MunifTanjim/nui.nvim",                        commit = "698e75814cd7c56b0dd8af4936bcef2d13807f3c" },
+  { "rcarriga/nvim-notify",                        commit = "159c6cf1be25a933f35e97499314c9faab55c98f" },
+  { "folke/noice.nvim",                            commit = "6a7649413ef61123c333f422ee95840c04ece161" },
+}
