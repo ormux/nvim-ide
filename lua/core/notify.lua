8924ec14ed8fa1297bf0be1cc43d7120fee86739
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-05-06 13:07:40 +1000

    add folke/noice.nvim for better aesthetics

---
+++ lua/core/notify.lua
@@ -0,0 +1,23 @@
+local status_ok, notify = pcall(require, "notify")
+if not status_ok then
+  vim.notify([[failed to load nvim-notify.]])
+  return
+end
+
+notify.setup({
+  background_colour = "NotifyBackground",
+  fps = 30,
+  icons = {
+    DEBUG = "",
+    ERROR = "",
+    INFO = "",
+    TRACE = "✎",
+    WARN = "",
+  },
+  level = 2,
+  minimum_width = 50,
+  render = "compact",
+  stages = "fade_in_slide_out",
+  timeout = 1000,
+  top_down = true,
+})
