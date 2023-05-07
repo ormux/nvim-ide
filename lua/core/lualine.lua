b0a2038161efbd9f818a63c061429570bd98ee8f
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-05-07 22:45:29 +1000

    add component to show macro recording status

--- lua/core/lualine.lua
+++ lua/core/lualine.lua
@@ -1,8 +1,22 @@
-local status_ok, lualine = pcall(require, "lualine")
+local lualine_status_ok, lualine = pcall(require, "lualine")
+local noice_status_ok, noice = pcall(require, "noice")
 
-if not status_ok then
+if not lualine_status_ok then
   vim.notify([[failed to load lualine.nvim]])
   return
+end
+
+if not noice_status_ok then
+  vim.notify([[failed to load noice.nvim]])
+  return
+end
+
+local function macro_recording()
+  local mode = noice.api.statusline.mode.get()
+  if mode then
+    return string.match(mode, "^recording @.*") or ""
+  end
+  return ""
 end
 
 lualine.setup({
@@ -16,9 +30,13 @@ lualine.setup({
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch", "diff", "diagnostics" },
-    lualine_c = { "filename" },
+    lualine_c = { "filename", {
+      macro_recording,
+      cond = noice.api.statusline.mode.has,
+      color = { fg = "#ff9e64" },
+    } },
     lualine_x = { "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
     lualine_z = { "location" },
-  }
+  },
 })
