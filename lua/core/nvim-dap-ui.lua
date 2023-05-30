480d6b7735d257cfbee16fe45e5f15a74f1c2b5c
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-05-30 15:43:40 +1000

    colorise nvim-dap breakpoints and logpoints
    - add custom unicode symbols for signs
    - custom function to clear breakpoints
    - clear breakpoints upon session exit

--- lua/core/nvim-dap-ui.lua
+++ lua/core/nvim-dap-ui.lua
@@ -30,3 +30,51 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
 	dapui.close()
 end
+
+-- https://github.com/mfussenegger/nvim-dap/discussions/355#discussioncomment-4398846
+vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#FF4444", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapBreakpointRejected", { ctermbg = 0, fg = "#FF0000", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapBreakpointCondition", { ctermbg = 0, fg = "#8316f7", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+vim.fn.sign_define(
+	"DapBreakpoint",
+	{ text = "●", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define("DapBreakpointCondition", {
+	text = "●",
+	texthl = "DapBreakpointCondition",
+	linehl = "DapBreakpointCondition",
+	numhl = "DapBreakpointCondition",
+})
+vim.fn.sign_define("DapBreakpointRejected", {
+	text = "",
+	texthl = "DapBreakpointRejected",
+	linehl = "DapBreakpointRejected",
+	numhl = "DapBreakpointRejected",
+})
+vim.fn.sign_define(
+	"DapLogPoint",
+	{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+)
+vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+
+vim.keymap.set("n", "<leader>dl", require("dap.ui.widgets").hover)
+
+-- utility functions
+local function dap_clear_breakpoints()
+	dap.clear_breakpoints()
+	vim.notify([[Breakpoints cleared]], "warn")
+end
+
+local function dap_terminate()
+	dap.clear_breakpoints()
+	dap.terminate({}, { terminateDebuggee = true }, function()
+		vim.notify("Debugger session ended", "warn")
+	end)
+end
+
+-- Customer Lua Keybindings
+vim.keymap.set("n", "<leader>dw", dap_clear_breakpoints)
+vim.keymap.set("n", "<leader>dq", dap_terminate)
