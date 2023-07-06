4c32c67945eafa8540d7e86297f3410454f6b54b
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-07-06 16:17:02 +1000

    enable lazy by default

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,25 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins_status_ok, plugins = pcall(require, "core.plugins")
 if not plugins_status_ok then
-  vim.notify([[failed to load plugins.lua]])
-  return
+	vim.notify([[failed to load plugins.lua]])
+	return
 end
 
 local lazy_status_ok, lazy = pcall(require, "lazy")
 if not lazy_status_ok then
-  vim.notify([[failed to load lazy.nvim]])
-  return
+	vim.notify([[failed to load lazy.nvim]])
+	return
 end
-lazy.setup(plugins)
+lazy.setup(plugins, { defaults = { lazy = true } })
