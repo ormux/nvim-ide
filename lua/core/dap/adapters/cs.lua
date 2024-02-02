a51e14f780a66cbfe918f7a5b92518b36268b01e
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-02-02 11:57:23 +1100

    fix cs DAP adapter

--- lua/core/dap/adapters/cs.lua
+++ lua/core/dap/adapters/cs.lua
@@ -5,12 +5,13 @@ if not status_ok then
 end
 
 -- https://github.com/mfussenegger/nvim-dap/wiki/Cookbook#making-debugging-net-easier
+-- https://stackoverflow.com/questions/77216161/how-to-setup-envs-in-neovim-dap-configs-for-dotnet-c
 vim.g.dotnet_build_project = function()
     local default_path = vim.fn.getcwd() .. '/'
     if vim.g['dotnet_last_proj_path'] ~= nil then
         default_path = vim.g['dotnet_last_proj_path']
     end
-    local path = vim.fn.input('Path to your *proj file', default_path, 'file')
+    local path = vim.fn.input('Path to *.csproj ', default_path, 'file')
     vim.g['dotnet_last_proj_path'] = path
     local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
     print('')
@@ -25,7 +26,8 @@ end
 
 vim.g.dotnet_get_dll_path = function()
     local request = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        -- return vim.fn.input('Path to dll file', vim.fn.getcwd(), '/bin/Debug/')
+        return vim.fn.input('Path to dll ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
     end
 
     if vim.g['dotnet_last_dll_path'] == nil then
@@ -39,28 +41,41 @@ vim.g.dotnet_get_dll_path = function()
     return vim.g['dotnet_last_dll_path']
 end
 
-local js_debug = mason_registry.get_package("netcoredbg")
-local server_path = js_debug:get_install_path()
+local netcoredbg = mason_registry.get_package("netcoredbg")
+local server_path = netcoredbg:get_install_path()
+vim.notify(server_path)
 
 return {
   name = "coreclr",
-adapter = {
-  type = 'executable',
-  command = server_path .. '/netcoredbg',
-  args = {'--interpreter=vscode'}
-},
-configuration = {
-  {
-    type = "coreclr",
-    name = "launch - netcoredbg",
-    request = "launch",
-    console = "externalTerminal",
-    program = function()
-        if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
-            vim.g.dotnet_build_project()
-        end
-        return vim.g.dotnet_get_dll_path()
-    end,
+  adapter = {
+		type = "executable",
+    command = server_path .. "/netcoredbg",
+    args = {'--interpreter=vscode'}
   },
-}
+  configuration = {
+    {
+			-- cwd = "${workspaceFolder}",
+   --    justMyCode = false,
+   --    stopAtEntry = false,
+
+      type = "coreclr",
+      name = "launch - netcoredbg",
+      request = "launch",
+      -- console = "externalTerminal",
+      program = function()
+          if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+              vim.g.dotnet_build_project()
+          end
+          return vim.g.dotnet_get_dll_path()
+      end,
+      -- env = {
+      --   ASPNETCORE_ENVIRONMENT = function()
+      --     return "Development"
+      --   end,
+      --   ASPNETCORE_URLS = function()
+      --     return "http://localhost:5050"
+      --   end,
+      -- },
+    },
+  }
 }
