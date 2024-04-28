c9708eb94f90a834b478efed5a1a23f14245ecbc
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-04-28 12:12:31 +1000

    add luarocks deps in rest-nvim

--- lua/tatum/lazy/plugins/rest-nvim.lua
+++ lua/tatum/lazy/plugins/rest-nvim.lua
@@ -2,9 +2,6 @@ return {
   "rest-nvim/rest.nvim",
   ft = "http",
   dependencies = {
-    { "vhyrro/luarocks.nvim",
-      priority = 1000,
-      config = true
-    },
+    "vhyrro/luarocks.nvim"
   }
 }
