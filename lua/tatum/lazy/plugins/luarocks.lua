e6d9d7fd397b8aec32511e0ece35024dbbdc3765
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-07-20 08:35:02 +1000

    remove rest-nvim version clamp, reformat luarocks.lua

--- lua/tatum/lazy/plugins/luarocks.lua
+++ lua/tatum/lazy/plugins/luarocks.lua
@@ -1,5 +1,8 @@
 return {
-  "vhyrro/luarocks.nvim",
-  priority = 1000,
-  config = true,
+	"vhyrro/luarocks.nvim",
+	priority = 1000,
+	opts = {
+		rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+	},
+	config = true,
 }
--- lua/tatum/lazy/plugins/rest-nvim.lua
+++ lua/tatum/lazy/plugins/rest-nvim.lua
@@ -1,6 +1,4 @@
 return {
 	"rest-nvim/rest.nvim",
-	ft = "http",
-	version = "v2.0.1",
 	dependencies = { "luarocks.nvim" },
 }
