565a6e13b562ac4f7fa7b788ee760f0921941a2b
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2024-07-20 08:31:12 +1000

    chore(telescope): fix typo null-ls to telescope

--- lua/tatum/lazy/config/telescope.lua
+++ lua/tatum/lazy/config/telescope.lua
@@ -1,7 +1,7 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok or telescope == nil then
-  vim.notify([[failed to load null-ls.]])
-  return
+	vim.notify([[failed to load telescope.]])
+	return
 end
 
-telescope.setup {}
+telescope.setup({})
